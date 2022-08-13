b16aa2bc3c0dca8e1774a9bf4990d2f6e81b322a
garu2 <elgafo.nakama@gmail.com>
2022-08-12 22:18:55 -0400

    ADD: Collections blog post

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ce3c606b336e6918827ee18582050df4'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd23be56c-1a08-4dac-91dc-ba4ce76ba837'
+};
